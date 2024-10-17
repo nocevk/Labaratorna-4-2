f8956f51ef09c3ad3a034811838a3cdd08aa5916
ТЕХНОРАЙ <@DESKTOP-E2H7NN2>
2024-10-17 18:12:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaratorna_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {   //Task2
+                Console.Write("Введіть розмір масиву: ");
+                int n = int.Parse(Console.ReadLine());
+
+                
+                int[] arr = new int[n];
+                Random rand = new Random();
+
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = rand.Next(5, 15);
+                }
+
+               
+                Console.WriteLine("Вихідний масив:");
+               
+                foreach (int num in arr)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+
+               
+                Console.WriteLine("Парні елементи та їх індекси:");
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 2 == 0)
+                    {
+                        Console.WriteLine($"Індекс: {i}, Значення: {arr[i]}");
+                    }
+                }
+            }
+        }
+    }
+    }
+
+
+
+
+
+
+
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Labaratorna 4,2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Labaratorna 4,2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e33b094c-3921-4758-af9d-eb99f720e4dd")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
